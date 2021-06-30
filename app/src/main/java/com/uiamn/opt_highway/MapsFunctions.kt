421e76bdbc4dcc9b895aef400cab0f65c2d4d005
NanohaTakamachi <yusukennh@gmail.com>
2021-06-30 20:01:40 +0900

    make searchMinumumPath return the path uncontained JCT

--- app/src/main/java/com/uiamn/opt_highway/MapsFunctions.kt
+++ app/src/main/java/com/uiamn/opt_highway/MapsFunctions.kt
@@ -11,6 +11,7 @@ import com.google.maps.GeoApiContext
 import org.json.JSONArray
 import java.io.BufferedReader
 import java.io.InputStreamReader
+import java.time.LocalTime
 
 class MapsFunctions(geoApiContext: GeoApiContext) {
     private val geoApiContext = geoApiContext
@@ -71,5 +72,9 @@ class MapsFunctions(geoApiContext: GeoApiContext) {
         val point = nearestIC.getJSONArray("point")
 
         return Structures.LatLngWithName(name, LatLng(point.getDouble(1), point.getDouble(0)))
+    }
+
+    fun obtainHighwaySection(icPath: ArrayList<String>, deptTime: LocalTime, arriveTime: LocalTime) {
+
     }
 }
